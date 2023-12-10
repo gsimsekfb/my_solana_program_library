b4ed1f67fbc2adcc0124d40cda039a188be44dd6
Gökhan Şimşek <g.simsek@paralect.com>
2023-12-10 17:20:34 +0300

    First working system fn: transfer_sol

--- src/system.rs
+++ src/system.rs
@@ -1,5 +1,10 @@
-use solana_program::account_info::AccountInfo;
+use solana_program::{
+    account_info::AccountInfo, 
+    // program_error::ProgramError,
+};
 
-pub fn foo() -> i32 {
-    4
+pub fn transfer_sol(from: &AccountInfo, to: &AccountInfo, amount: u64) {
+// -> Result<std::cell::RefMut<&mut u64>, ProgramError> {
+    **from.try_borrow_mut_lamports().unwrap() -= amount; // todo use "?" op.
+    **to.try_borrow_mut_lamports().unwrap() += amount;
 }
