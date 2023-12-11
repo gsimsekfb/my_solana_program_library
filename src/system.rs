265bfb6c5666ecf99a8c14aab884b1c70e1710be
Gökhan Şimşek <g.simsek@paralect.com>
2023-12-11 17:19:57 +0300

    Adding result return

--- src/system.rs
+++ src/system.rs
@@ -1,10 +1,11 @@
 use solana_program::{
-    account_info::AccountInfo, 
-    // program_error::ProgramError,
+    account_info::AccountInfo, entrypoint::ProgramResult,
 };
 
-pub fn transfer_sol(from: &AccountInfo, to: &AccountInfo, amount: u64) {
-// -> Result<std::cell::RefMut<&mut u64>, ProgramError> {
-    **from.try_borrow_mut_lamports().unwrap() -= amount; // todo use "?" op.
-    **to.try_borrow_mut_lamports().unwrap() += amount;
+pub fn transfer_sol(
+    from: &AccountInfo, to: &AccountInfo, amount: u64
+) -> ProgramResult {
+    **from.try_borrow_mut_lamports()? -= amount;
+    **to.try_borrow_mut_lamports()? += amount;
+    Ok(())
 }
